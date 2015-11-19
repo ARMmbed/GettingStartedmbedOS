1860b7d7513a464a148ce7bf71abfa3be554e6dc
Rob Moran <github@thegecko.org>
2015-11-19 20:29:49 +0000

    Changed script to mount directory with same name as current directory
    This change mitigates a problem with ```yotta init``` thinking all new projects are called ```workspace```

--- Docs/Scripts/yotta.sh
+++ Docs/Scripts/yotta.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
+folder=`basename $PWD`
+
 PARAMS="$PARAMS -it --rm"
-PARAMS="$PARAMS -w /workspace"
-PARAMS="$PARAMS -v $PWD:/workspace"
+PARAMS="$PARAMS -w /$folder"
+PARAMS="$PARAMS -v $PWD:/$folder"
 PARAMS="$PARAMS --entrypoint yotta"
 
 docker run $PARAMS mbed/yotta $@
